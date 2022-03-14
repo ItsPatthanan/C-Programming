5fdf99b8e69c7b0eb7c096e4ed8aa14daf6705bc
tumlungaen <mr.kittinan@gmail.com>
2022-03-14 15:30:25 +0700

    try to fix e-lab-8-fix.c, last bug stand.

--- ENGCC304-Upady/e-lab-8-fix.c
+++ ENGCC304-Upady/e-lab-8-fix.c
@@ -1,39 +1,39 @@
 #include <stdio.h>
- 
+#include <string.h> //FIX : ADD string.h here.
  
 void main()
 {
     char CustomerName[15],Name[10][15];
     int Quantity[10],NumberOfItems,i;
     float UnitPrice[10],TotalPrice[10],TotalAmount;
- 
+
     printf("Enter customer name [QUIT to stop] : ");
-    scanf("%s",CustomerName)
+    scanf("%s",CustomerName) ; //FIX : ADD ;
  
-    while(strcmp(Customername,"QUIT") != 0){
+    while(strcmp(CustomerName,"QUIT") != 0){ //FIX : CHANGE 'Customername' to 'CustomerName' 
         i = 0;
         printf("Enter name, quantity, and price for item %d [END 0 0] : ", i+1);
-        Scanf("%s %d %f", Name[i],  Quantity[i], &UnitPrice[i]);
+        scanf("%s %d %f", Name[i],  Quantity[i], &UnitPrice[i]); //FIX : CHANGE 'Scanf' to 'scanf'
  
         while(strcmp(Name[i],"END") != 0){
             i++;
             printf("Enter name, quantity, and price for item %d [END 0 0] : ", i+1);
             scanf("%s %d %f", Name[i], &Quantity[i], &UnitPrice[i]);
-         
+        }//FIX : ADD } here.
         NumberOfItems = i;
  
-        TotalAmount = 0.0
+        TotalAmount = 0.0 ; //FIX : ADD ; here.
  
         for(i=0;i<NumberOfItems;i++){
             TotalPrice[i] = Quantity[i]*UnitPrice[i];
             TotalAmount += TotalPrice[i];
         }
  
-        Printf("--------------------------------------------------------------\n");
-        printf("%-20s %11s %13s %17s\n",  Name", "Quantity", "Unit Price", "Total Price");
+        printf("--------------------------------------------------------------\n"); //FIX : CHANGE 'Printf' to 'printf'
+        printf("%-20s %11s %13s %17s\n",  "Name", "Quantity", "Unit Price", "Total Price"); //FIX : ADD Double Quote before Name variable.
         printf("--------------------------------------------------------------\n");
  
-        for(i=0;i<NumberOfItems;i++);
+        for(i=0;i<NumberOfItems;i++) //FIX : DELETE ; before )
             printf("%-20s %11.2f %13d %17.2f\n", Name[i], Quantity[i] , UnitPrice[i], TotalPrice[i]);
  
         printf("--------------------------------------------------------------\n");
@@ -42,4 +42,4 @@ void main()
         scanf("%s",CustomerName);
     }
     printf("++++++++++SYSTEM SHUTDOWN+++++++++++\n");
-}
+} 
